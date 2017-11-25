0ca6073253d3bbb83df8b940483f24d301e7de9a
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-25 19:01:29 +1000

    Ended the Debugging experiment.

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   
-
+    // MARK: - Action Methods
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Called buttonTapped(_:)")
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        // Go one step too far emptying the array (range changed)
+        for _ in 0..<10 {
+            array.removeObject(at: 0)
+        }
+    }
+    
 
 }
 
